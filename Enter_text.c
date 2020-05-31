5edcdf4f5702d5ee4217ace99a7a93f54256fe02
He3k_Win10 <Cloud.He3k@yandex.ru>
2020-05-31 11:30:17 +0700

    Update Enter & Print text

--- Enter_text.c
+++ Enter_text.c
@@ -1,25 +1,24 @@
-#include <stdio.h> 
+#include <stdio.h>
 
 int main() {
 
-	FILE *fp;
+	char name[20];
+	char enter_array; //объ€вление символьной переменной ch
+	printf("Vvedite nazvanie faila, soderjimoe kotorogo hotite otkryt`\n");
+	scanf("%s", name);
 
-	char ch; //объ€вление символьной переменной ch
-	printf("Vvodite lubie simvoly, krome $ \n\n");
+	FILE *enter_text;
 
-	if ((fp = fopen("test.txt", "w")) == NULL) {
+	if ((enter_text = fopen(name, "r")) == NULL) {
 		printf("Oshibka pri otkrytii faila!\n");
 	}
-
-	do{
-
-ch = getchar();
-putc(ch, fp);
-
-	} 
-while (ch != '0');
-	fclose(fp);
-
-return 0;
-
+	enter_array = getc(enter_text); //чтение одного символа
+	
+	while (enter_array != EOF) {//пока не достигли конца документа
+		putchar(enter_array); //выводим на экран
+		enter_array = getc(enter_text);
+	}
+	fclose(enter_text);
+	
+	return 0;
 }
--- Print_text.c
+++ Print_text.c
@@ -1,24 +1,25 @@
-#include <stdio.h>
+#include <stdio.h> 
 
 int main() {
 
-	char name[20];
-	char ch; //объ€вление символьной переменной ch
-	printf("Vvedite nazvanie faila, soderjimoe kotorogo hotite otkryt`\n");
-	scanf("%s", name);
+	FILE *print_array;
 
-	FILE *fp;
+	char print_text; //объ€вление символьной переменной ch
+	printf("Vvodite lubie simvoly, krome $ \n\n");
 
-	if ((fp = fopen(name, "r")) == NULL) {
+	if ((print_array = fopen("test.txt", "w")) == NULL) {
 		printf("Oshibka pri otkrytii faila!\n");
 	}
-	ch = getc(fp); //чтение одного символа
-	
-	while (ch != EOF) {//пока не достигли конца документа
-		putchar(ch); //выводим на экран
-		ch = getc(fp);
-	}
-	fclose(fp);
-	
-	return 0;
+
+	do{
+
+print_text = getchar();
+putc(print_text, print_array);
+
+	} 
+while (print_text != '0');
+	fclose(print_array);
+
+return 0;
+
 }
